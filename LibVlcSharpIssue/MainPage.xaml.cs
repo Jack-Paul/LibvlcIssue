612d60e031d4877671de4e18ccd159cfb8cd2adf
jack_paul <jackpaul3413@gmail.com>
2022-12-08 15:08:34 +0530

    log added

--- LibVlcSharpIssue/MainPage.xaml.cs
+++ LibVlcSharpIssue/MainPage.xaml.cs
@@ -13,15 +13,29 @@ namespace LibVlcSharpIssue
     {
         public LibVLC libVLC;
         public MediaPlayer audio;
+        public string Log;
         public MainPage()
         {
             InitializeComponent();
             Core.Initialize();
-            libVLC = new LibVLC();
+            libVLC = new LibVLC(enableDebugLogs: true);
 
             audio = new MediaPlayer(libVLC);
             audio.EncounteredError += Audio_EncounteredError;
+            libVLC.Log += LibVLC_Log;
         }
+
+        private void LibVLC_Log(object sender, LogEventArgs e)
+        {
+            Log +="Time" + DateTime.Now + System.Environment.NewLine +
+                "Level" + e.Level + System.Environment.NewLine +
+                "Message:" + e.Message + System.Environment.NewLine +
+                "Module:" + e.Module + System.Environment.NewLine +
+                "Soruce File" + e.SourceFile + System.Environment.NewLine +
+                "Source Line" + e.SourceLine + System.Environment.NewLine + System.Environment.NewLine
+                ;
+        }
+
         private void Audio_EncounteredError(object sender, EventArgs e)
         {
             Device.BeginInvokeOnMainThread(async () =>
@@ -31,11 +45,27 @@ namespace LibVlcSharpIssue
         }
         private void Button_Clicked(object sender, EventArgs e)
         {
+            audio.Stop();
             string url = "https://mangoradio.stream.laut.fm/mangoradio?t302=2022-12-07_13-54-16&uuid=e330361f-f9fe-4734-a64a-8adc5e30f9bb";
             var media = new Media(libVLC, url, FromType.FromLocation);
             audio.Media = media;
             audio.Media.AddOption(":no-video");
             audio.Play();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            audio.Stop();
+            string url = "https://live.trucksim.fm/";
+            var media = new Media(libVLC, url, FromType.FromLocation);
+            audio.Media = media;
+            audio.Media.AddOption(":no-video");
+            audio.Play();
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            var s = Log;
         }
     }
 }
